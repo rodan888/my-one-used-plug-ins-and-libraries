7f5b9e434cd461f0934360be56a6280a96e33e64
oleksandr <tsumbaluk888@gmail.com>
2015-04-20 11:37:10 +0300

    konkyrs verstka

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay:3500, //Set AutoPlay to 3 seconds
+ 
+      items : 3,
+      itemsDesktop : [1142,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+});
+
+
+ 
